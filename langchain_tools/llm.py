a2e6681c10ccf0d283e8f8d74415eb57c325f222
Mongar28 <cristian.mongar@onecluster.org>
2024-07-19 16:28:26 -0500

    Se agrega el nuevo modelo gpt-4o-mini a las opciones de los modelos

--- langchain_tools/llm.py
+++ langchain_tools/llm.py
@@ -4,8 +4,9 @@ from langchain_openai import ChatOpenAI
 
 def load_llm_openai():
     load_dotenv()
-    model = "gpt-3.5-turbo-0125"
+    # model = "gpt-3.5-turbo-0125"
     # model = "gpt-4o"
+    model = "gpt-4o-mini"
 
     llm = ChatOpenAI(
         model=model,
